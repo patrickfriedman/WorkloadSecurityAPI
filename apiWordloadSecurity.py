45123050e892658113ebd5f23d04511ec7ac169f
Patrick Friedman <patrick.s.friedman@gmail.com>
2021-06-25 10:51:13 -0500

    Add files via upload
    Changes to text and play a sound when parameter changes

--- apiWordloadSecurity.py
+++ apiWordloadSecurity.py
@@ -2,11 +2,29 @@ from __future__ import print_function
 import sys, warnings
 import deepsecurity
 from deepsecurity.rest import ApiException
+from playsound import playsound
+from twilio.rest import Client
 import csv
+import time
+# play a sound when a parameter changes and send a message
+lists = []
+
+def text():
+	account_sid = 'AC20aef97ee2405b304d03111b5ae30439'
+	auth_token = 'ae2682f547d1235d992c4bd32917f5aa'
+	client = Client(account_sid, auth_token)
+	message = client.messages \
+						.create(
+								body="Some unauthorized changes were made to your agent: " + str(lists),
+								from_='+19725034157',
+								to='+12144713159'
+						)
+	print(message.sid)
 
 def write(list):
 	count = 0
 	computers = 0
+	ec2 = 0
 	sourceFile = open('api.csv', 'w')
 
 	csvreader = csv.writer(sourceFile, delimiter=',')
@@ -14,6 +32,7 @@ def write(list):
 	
 	api = str(list).split('\n')
 	api.pop(0)
+
 	for i in api:
 		print(i)
 		sourceFile.writelines(i)
@@ -27,6 +46,34 @@ def write(list):
 			print('\n')
 			csvreader.writerow([])
 			computers = 0
+			ec2 = ec2 + 1
+	
+	csvreader.writerow([])
+	csvreader.writerow(['COMPUTER NAME', 'PLATFORM', 'POLICY NAME', 'STATUS'])
+
+	flag = 0
+	for x in range(ec2 + 1): 
+		flag = flag + 1
+		name = api.pop((x * 38) + 12)
+		sourceFile.write(name)
+
+		platform = api.pop((x * 38) + 30)
+		sourceFile.write(platform)
+
+		policyname = api.pop((x * 38) + 4)
+		sourceFile.write(policyname)
+
+		status = api.pop((x * 38) + 1)
+		sourceFile.write(status)
+
+		sourceFile.write('\n')
+		flag = True
+
+		if(flag == 1):
+			lists.append(name)
+			lists.append(platform)
+			lists.append(policyname)
+			lists.append(status)
 
 	sourceFile.close()
 
@@ -38,7 +85,7 @@ def list():
 	configuration.host = 'https://cloudone.trendmicro.com/api'
 
 	# Authentication
-	configuration.api_key['api-secret-key'] = '21283D1A-197B-5B20-2F0F-2B71586061A9:5E986039-3478-SFEE-AF5A-FD2E4478A830:rCmYF6SEd6hiOwUSNXePB2M12dfFHer6KopDzOzlDic='
+	configuration.api_key['api-secret-key'] = '22A83D1A-197B-5B20-2F0F-2B7B84A061A9:5E986039-3C78-8CCE-AF5A-FD2E4478A830:rCmYF6SEd6hiOwUSNXePB2MGI39SHer6KopDzOzlDic='
 
 	# Initialization
 	# Set Any Required Values
@@ -50,38 +97,23 @@ def list():
 	overrides = False
 
 	try:
-		api_responce = api_instance.list_computers(api_version, expand=expand, overrides=overrides)
-		return api_responce
+		api_response = api_instance.list_computers(api_version, expand=expand, overrides=overrides)
+		return api_response
 
 	except ApiException as e:
 		print("An exception occurred when calling ComputersApi.list_computers: %s\n" % e)
 
-def description(): # couldnt get it to work via hostname
-	# Setup
-	if not sys.warnoptions:
-		warnings.simplefilter("ignore")
-	configuration = deepsecurity.Configuration()
-	configuration.host = 'https://cloudone.trendmicro.com/api/computers/{17}'
+noerror = True
+while(noerror):
+	api_response = list()
+	write(api_response)
+	time.sleep(5)
+	lists = []
+	newapi = list()
+	write(newapi)
 
-	# Authentication
-	configuration.api_key['api-secret-key'] = '21283D1A-197B-5B20-2F0F-2B71586061A9:5E986039-3478-SFEE-AF5A-FD2E4478A830:rCmYF6SEd6hiOwUSNXePB2M12dfFHer6KopDzOzlDic='
-
-	# Initialization
-	# Set Any Required Values
-	api_instance = deepsecurity.ComputersApi(deepsecurity.ApiClient(configuration))
-	computer_id = 1
-	api_version = 'v1'
-	expand_options = deepsecurity.Expand()
-	expand_options.add(expand_options.none)
-	expand = expand_options.list()
-	overrides = False
-
-	try:
-		api_response = api_instance.describe_computer(computer_id, api_version, expand=expand, overrides=overrides)
-		return api_response
-	except ApiException as e:
-		print("An exception occurred when calling ComputersApi.describe_computer: %s\n" % e)
-
-
-write(list())
-# write(description())
+	if(api_response != newapi):
+		playsound('/Users/Koolk/OneDrive/Desktop/UNT/Putty Backup/Personal Projects/CPITs/startup.mp3')
+		print("\n" + str(lists) + "\n")
+		text()
+		noerror = False
